e5bfd6665ed46bd266f578b8aac7a18c81f05584
itMarzola <nandomarzola1@gmail.com>
2025-06-11 16:00:59 -0300

    feat: add footer in boottom

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,9 @@
 export default function Footer() {
-    return (
-      <footer className="w-full h-24 bg-[#7b1f24] flex items-center justify-center relative shadow-inner-footer">
-        <p className="text-white text-sm">
-          &copy; {new Date().getFullYear()} ECOMZERO. Todos os direitos reservados.
-        </p>
-      </footer>
-    );
-  }
-  
+  return (
+    <footer className="w-full h-24 bg-[#7b1f24] flex items-center justify-center shadow-inner-footer fixed bottom-0 left-0 z-50">
+      <p className="text-white text-sm">
+        &copy; {new Date().getFullYear()} ECOMZERO. Todos os direitos reservados.
+      </p>
+    </footer>
+  );
+}
